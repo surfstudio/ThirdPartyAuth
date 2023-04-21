8e0961db5f86d07971747ad50058e8e02ab2c2a6
Ilya Klimenyuk <ilya.klimenyuk@gmail.com>
2023-04-21 18:05:51 +0300

    Add apple auth provider and service for check user credentials state

--- ThirdPartyAuth/Apple/Providers/AppleAuthProvider.swift
+++ ThirdPartyAuth/Apple/Providers/AppleAuthProvider.swift
@@ -5,4 +5,41 @@
 //  Created by Ilya Klimenyuk on 21.04.2023.
 //
 
-import Foundation
+import AuthenticationServices
+
+final class AppleAuthProvider: NSObject, BaseAuthProvider {
+
+    // MARK: - Properties
+
+    var onAuthFinished: ((ThirdPartyAuthResult) -> Void)?
+
+    // MARK: - Methods
+
+    func performAuth() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
+    }
+
+}
+
+// MARK: - ASAuthorizationControllerDelegate
+
+extension AppleAuthProvider: ASAuthorizationControllerDelegate {
+
+    func authorizationController(controller: ASAuthorizationController,
+                                 didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let userModel = ThirdPartyAuthUserModel(from: appleIDCredential)
+            onAuthFinished?(.success(userModel))
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        onAuthFinished?(.failure(error))
+    }
+
+}
--- ThirdPartyAuth/Apple/Services/AppleCredentialsService.swift
+++ ThirdPartyAuth/Apple/Services/AppleCredentialsService.swift
@@ -5,4 +5,23 @@
 //  Created by Ilya Klimenyuk on 21.04.2023.
 //
 
-import Foundation
+import AuthenticationServices
+
+public final class AppleCredentialsService: BaseCredentialsService {
+
+    // MARK: - Public Methods
+
+    public func checkCredentialsState(for userID: String, _ onCheckCredentialsValid: @escaping (Bool) -> Void) {
+        guard !userID.isEmpty else {
+            onCheckCredentialsValid(false)
+            return
+        }
+
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        appleIDProvider.getCredentialState(forUserID: userID) { (credentialState, _) in
+            let isValid = credentialState == .authorized
+            onCheckCredentialsValid(isValid)
+        }
+    }
+
+}
