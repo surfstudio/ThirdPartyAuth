f158d68c22a18513de3e02a2a569de7e972725a8
Ilya Klimenyuk <ilya.klimenyuk@gmail.com>
2023-04-22 10:46:49 +0300

    Add SurfUtils library to dependencies

--- Package.swift
+++ Package.swift
@@ -11,10 +11,18 @@ let package = Package(
             name: "ThirdPartyAuth",
             targets: ["ThirdPartyAuth"])
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/surfstudio/iOS-Utils.git",
+            revision: "13.2.0"
+        )
+    ],
     targets: [
         .target(
             name: "ThirdPartyAuth",
-            dependencies: [],
+            dependencies: [
+                .product(name: "Utils", package: "iOS-Utils")
+            ],
             path: "ThirdPartyAuth"
         )
     ]
